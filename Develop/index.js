679b5a4812ead2e51211f8270c1810a543375669
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-15 12:17:50 -0500

    wrote inquirer prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,59 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const markdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Please write a brief description of your project: ",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What command is needed to install your project?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "Please define any contribution guidelines: ",
+        name: "contribution",
+    },
+    {
+        type: "list",
+        message: "Please select a license",
+        name: "license",
+        choices: ["MIT", "GNU GPLv3", "None"]
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("README.md")
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => console.log(response));
+}
 
 // Function call to initialize app
 init();
